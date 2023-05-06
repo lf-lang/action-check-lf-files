71e6e7877f7c9ad4f1a2818fab191ba9597f6ecd
Marten Lohstroh <marten@berkeley.edu>
2023-05-06 14:08:36 -0700

    Check out the lingua-franca repo

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {simpleGit} from 'simple-git'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const ref: string = core.getInput('compiler-ref')
+    const dir: string = core.getInput('checkout-dir')
+    const git = simpleGit()
+    git.clone('git@github.com:lf-lang/lingua-franca.git', dir)
+    git.checkoutLocalBranch(ref) // FIXME: how to point it to the right directory?
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Build compiler and set command
+    
+    // Traverse tree
 
-    core.setOutput('time', new Date().toTimeString())
+
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
+
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
