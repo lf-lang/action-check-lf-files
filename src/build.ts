aa67f7fa156e64f58cafe50b3770131a2edfbef7
Christian Menard <Christian.Menard@gmx.de>
2023-06-06 11:02:56 +0200

    Update build.ts

--- src/build.ts
+++ src/build.ts
@@ -5,19 +5,6 @@ import * as path from 'node:path'
 import {promisify} from 'util'
 
 const exec = promisify(child_process.exec)
-let myOutput = '';
-let myError = '';
-
-const options = {
-listeners: {
-stdout: (data: Buffer) => {
-myOutput += data.toString();
-},
-stderr: (data: Buffer) => {
-myError += data.toString();
-}
-}
-};
 
 export async function deleteIfExists(dir: string): Promise<void> {
   return rm(dir, {recursive: true, force: true})
@@ -30,14 +17,24 @@ export async function clone(ref: string, dir: string): Promise<void> {
 
 export async function build(dir: string): Promise<void> {
   const dir_up = `${dir}/..`
-  await exec('ls -la', {cwd: dir, stdout: (data: Buffer) => { myOutput += data.toString(); }, stderr: (data: Buffer) => { myError += data.toString(); }})
+  await exec('ls -la', {cwd: dir},
+   function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+          console.log('exec error: ' + error);
+      }
+   })
   console.log(myOutput);
   console.log(myError);
-  await exec('ls -la', {cwd: dir_up, stdout: (data: Buffer) => { myOutput += data.toString(); }, stderr: (data: Buffer) => { myError += data.toString(); }})
-  myOutput = ''
-  myError = ''
-  console.log(myOutput);
-  console.log(myError);
+  await exec('ls -la', {cwd: dir_up},
+   function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+          console.log('exec error: ' + error);
+      }
+   })
   exec('./gradlew clean assemble', {cwd: dir})
 }
 
